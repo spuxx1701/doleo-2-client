cef485972e315aa4e1b7cb3fe31f1899ea22b7ca
spuxx1701 <ameisenfutter@outlook.com>
2022-08-18 21:23:37 +0200

    introduced bootstrap

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
 import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'doleo-2-client/config/environment';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
