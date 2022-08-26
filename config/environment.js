215ee12e3d704fa2c7cb513e94c4b503689a92a3
spuxx1701 <ameisenfutter@outlook.com>
2022-08-26 09:41:06 +0200

    ember did some stuff to tests

--- config/environment.js
+++ config/environment.js
@@ -27,24 +27,14 @@ module.exports = function (environment) {
   };
 
   if (environment === 'development') {
-    ENV.apiUrl = 'http://localhost:3000';
-    // ENV.APP.LOG_RESOLVER = true;
     ENV.APP.LOG_ACTIVE_GENERATION = true;
     ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
-
+  if (environment === 'integration') {
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
-
-    ENV.APP.rootElement = '#ember-testing';
-    ENV.APP.autoboot = false;
   }
 
   if (environment === 'production') {
