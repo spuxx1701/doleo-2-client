d8460bb8785271dc8a15010bbc1658676c35aa3f
Spuxx <ameisenfutter@outlook.com>
2022-09-12 18:30:20 +0200

    remove button

--- app/controllers/authenticated/index.ts
+++ app/controllers/authenticated/index.ts
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 import WebNotificationService from 'doleo-2-client/services/web-notification';
 import ManagerService from 'doleo-2-client/services/manager';
 import AccountService from 'doleo-2-client/services/account';
@@ -9,10 +8,4 @@ export default class IndexController extends Controller {
   @service declare webNotification: WebNotificationService;
   @service declare manager: ManagerService;
   @service declare account: AccountService;
-
-  @action createNotification() {
-    const title = 'Hello World';
-    const message = 'I am a notification!';
-    this.webNotification.create(title, message);
-  }
 }
