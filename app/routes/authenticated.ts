1510cc091f3b369287dad0e6baaccae0526f0292
Spuxx <ameisenfutter@outlook.com>
2022-09-13 20:26:51 +0200

    fixed authentication & routing

--- app/routes/authenticated.ts
+++ app/routes/authenticated.ts
@@ -4,11 +4,17 @@ import { inject as service } from '@ember/service';
 import ManagerService from 'doleo-2-client/services/manager';
 import AccountService from 'doleo-2-client/services/account';
 import RSVP from 'rsvp';
+import transition from '@ember/routing/transition';
 
 export default class ProtectedRoute extends Route {
   @service declare store: Store;
+  @service declare session: any;
   @service declare manager: ManagerService;
   @service declare account: AccountService;
+
+  async beforeModel(transition: transition<unknown>) {
+    this.session.requireAuthentication(transition, 'login');
+  }
 
   async model() {
     await this.account.load();
