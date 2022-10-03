0bf88127e578053b0c5fa3479bda275bb8946a41
Spuxx <ameisenfutter@outlook.com>
2022-10-03 18:06:47 +0200

    fixed route name

--- app/routes/authenticated/index.ts
+++ app/routes/authenticated/index.ts
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import NewsFeedService from 'doleo-2-client/services/news-feed';
 
-export default class SettingsRoute extends Route {
+export default class HomeRoute extends Route {
   @service declare newsFeed: NewsFeedService;
 
   async model() {
