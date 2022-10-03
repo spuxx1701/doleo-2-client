20950e99f429afa95082e2119e474054284feae6
Spuxx <ameisenfutter@outlook.com>
2022-10-03 18:10:31 +0200

    force unload list-invites

--- app/services/news-feed.ts
+++ app/services/news-feed.ts
@@ -12,6 +12,9 @@ export default class NewsFeedService extends Service {
   @tracked listInvites: Ember.ArrayProxy<ListInvite> | undefined;
 
   @action async refresh() {
+    // Force a complete refresh on list-invites so we don't accidentally display invites
+    // that were created in this session
+    this.store.unloadAll('list-invite');
     this.listInvites = await this.store.findAll('list-invite');
     this.update();
   }
