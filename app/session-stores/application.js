40098e8a9c6385c1d75c7f2a1c21e4797a58fbe9
Spuxx <ameisenfutter@outlook.com>
2022-10-06 12:08:07 +0200

    set cookie expiration time

--- app/session-stores/application.js
+++ app/session-stores/application.js
@@ -1,3 +1,5 @@
 import Cookie from 'ember-simple-auth/session-stores/cookie';
 
-export default class ApplicationSessionStore extends Cookie {}
+export default class ApplicationSessionStore extends Cookie {
+  cookieExpirationTime = 14 * 24 * 60 * 60;
+}
