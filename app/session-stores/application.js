2db089d16e3cf267013b4a1861bc892cc911b448
Spuxx <ameisenfutter@outlook.com>
2022-10-10 13:24:57 +0200

    set samesite attribute

--- app/session-stores/application.js
+++ app/session-stores/application.js
@@ -2,4 +2,5 @@ import Cookie from 'ember-simple-auth/session-stores/cookie';
 
 export default class ApplicationSessionStore extends Cookie {
   cookieExpirationTime = 14 * 24 * 60 * 60;
+  sameSite = 'strict';
 }
