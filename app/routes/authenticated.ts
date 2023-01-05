c1eed27d77888eb77994fb96c7fb2dfcb5f8e7c6
Spuxx <ameisenfutter@outlook.com>
2023-01-05 18:07:45 +0100

    try to prevent authentication failure on startup

--- app/routes/authenticated.ts
+++ app/routes/authenticated.ts
@@ -5,15 +5,21 @@ import AccountService from 'doleo-2-client/services/account';
 import RSVP from 'rsvp';
 import transition from '@ember/routing/transition';
 import CustomStore from 'doleo-2-client/services/custom-store';
+import RouterService from '@ember/routing/router-service';
 
 export default class ProtectedRoute extends Route {
+  @service declare router: RouterService;
   @service declare store: CustomStore;
   @service declare session: any;
   @service declare manager: ManagerService;
   @service declare account: AccountService;
 
   async beforeModel(transition: transition<unknown>) {
-    this.session.requireAuthentication(transition, 'login');
+    try {
+      this.session.requireAuthentication(transition, 'login');
+    } catch (error) {
+      this.router.transitionTo('login');
+    }
   }
 
   async model() {
