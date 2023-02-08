e9b1cb9e38b7c8529f93ba5e36b640da8d3085e8
Spuxx <ameisenfutter@outlook.com>
2023-02-08 17:17:32 +0100

    enabled PWA mode

---
+++ config/manifest.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = function (/* environment, appConfig */) {
+  // See https://zonkyio.github.io/ember-web-app for a list of
+  // supported properties
+
+  return {
+    name: 'Doleo',
+    short_name: 'Doleo',
+    description: 'The Doleo progressive web app.',
+    start_url: '/',
+    scope: '/',
+    display: 'standalone',
+    background_color: '#ffe1e1',
+    theme_color: '#d87093',
+    icons: [
+      {
+        src: 'assets/logo/100x100.png',
+        sizes: '100x100',
+        type: 'image/png',
+      },
+    ],
+    ms: {
+      tileColor: '#d87093',
+    },
+  };
+};
